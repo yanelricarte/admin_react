1e48f41b24fdac5a0eecbf89ec60c2006fa2b510
yricarte <ryanel@indracompany.com>
2024-11-06 12:37:33 -0300

    puerto front

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Permite acceder a la aplicación desde cualquier IP
+    port: 3000,       // Define el puerto en el que debe escuchar la app
+  },
 })
